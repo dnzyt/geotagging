9dd10ebeb0388959b9ed9ed12567ca610b0b6b37
ningzeda <ningzeda@herbalife.com>
2021-10-02 11:54:29 -0400

    initial commit

---
+++ geotag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  geotag
+//
+//  Created by Ningze Dai on 9/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
